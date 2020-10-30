7113827eac0c2a5e348e1238914c5991265944cb
koturn <jeak.koutan.apple@gmail.com>
2020-10-30 16:26:53 +0900

    Improve generating documents for Qt

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -18,6 +18,7 @@ function(add_doxygen)
     OUTPUT_DIRECTORY)
   set(multiValueArgs
     TARGETS
+    QT_TARGETS
     SOURCES
     INCLUDE_DIRECTORIES
     COMPILE_DEFINITIONS)
@@ -35,7 +36,7 @@ function(add_doxygen)
     set(DOXYGEN_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/document")
   endif()
 
-  foreach(target ${DOXYGEN_TARGETS})
+  foreach(target ${DOXYGEN_TARGETS} ${DOXYGEN_QT_TARGETS})
     get_property(sources
       TARGET ${target}
       PROPERTY SOURCES)
@@ -50,6 +51,14 @@ function(add_doxygen)
       DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
       PROPERTY COMPILE_DEFINITIONS)
     list(APPEND DOXYGEN_COMPILE_DEFINITIONS ${defs})
+  endforeach(target)
+
+  set(abs_autogen_source_paths "")
+  foreach(target ${DOXYGEN_QT_TARGETS})
+    file(GLOB_RECURSE autogen_sources
+      "${CMAKE_CURRENT_BINARY_DIR}/${target}_autogen/*.cpp"
+      "${CMAKE_CURRENT_BINARY_DIR}/${target}_autogen/*.h")
+    list(APPEND abs_autogen_source_paths ${autogen_sources})
   endforeach(target)
 
   # Convert to absolute path.
@@ -85,10 +94,20 @@ function(add_doxygen)
   endforeach(incdir)
   list(REMOVE_DUPLICATES abs_incdir_header_paths)
 
-  set(outputdir ${CMAKE_CURRENT_BINARY_DIR}/DoxyDoc)
+  set(abs_input_dir_paths "")
+  foreach(input_dir ${DOXYGEN_INPUT_DIRECTORIES})
+    if(IS_ABSOLUTE "${input_dir}")
+      list(APPEND abs_input_dir_paths "${input_dir}")
+    else()
+      get_filename_component(abs_input_dir "${input_dir}" ABSOLUTE)
+      list(APPEND abs_input_dir_paths "${abs_input_dir}")
+    endif()
+  endforeach()
+  message(STATUS "abs_input_dir_paths: ${abs_input_dir_paths}")
 
   # Convert lists to whitespace separated strings
   string(REPLACE ";" " " abs_source_paths_ws_splited "${abs_source_paths}")
+  string(REPLACE ";" " " abs_autogen_source_paths_ws_splited "${abs_autogen_source_paths}")
   string(REPLACE ";" " " abs_incdir_header_paths_ws_splited "${abs_incdir_header_paths}")
   string(REPLACE ";" " " abs_incdir_paths_ws_splited "${INCLUDE_DIRECTORIES}")
   string(REPLACE ";" " " defs_ws_splited "${DOXYGEN_COMPILE_DEFINITIONS}")
@@ -104,7 +123,7 @@ function(add_doxygen)
     OUTPUT "${DOXYGEN_OUTPUT_DIRECTORY}/${DOXYGEN_NAME}/Doxyfile"
     COMMAND ${CMAKE_COMMAND}
       -D "DOXYGEN_TEMPLATE=${DOXYGEN_CURRENT_LIST_DIR}/templates/Doxyfile.in"
-      -D "DOXY_PROJECT_INPUT=${abs_source_paths_ws_splited} ${abs_incdir_header_paths_ws_splited}"
+      -D "DOXY_PROJECT_INPUT=${abs_source_paths_ws_splited} ${abs_incdir_header_paths_ws_splited} ${abs_autogen_source_paths_ws_splited} "
       -D "DOXY_PROJECT_INCLUDE_DIR=${abs_incdir_paths_ws_splited}"
       -D "DOXY_PROJECT_PREDEFINED=${predef_spaces}"
       -D "DOXY_PROJECT_STRIP_FROM_PATH=${CMAKE_SOURCE_DIR}"
@@ -126,6 +145,8 @@ function(add_doxygen)
   add_custom_target(doxygen-${DOXYGEN_NAME}
     DEPENDS "${DOXYGEN_OUTPUT_DIRECTORY}/${DOXYGEN_NAME}/index.html")
 
-  add_dependencies(doxygen
-    doxygen-${DOXYGEN_NAME})
+  add_dependencies(doxygen doxygen-${DOXYGEN_NAME})
+  foreach(target ${DOXYGEN_QT_TARGETS})
+    add_dependencies(doxygen-${DOXYGEN_NAME} "${target}_autogen")
+  endforeach(target)
 endfunction()
